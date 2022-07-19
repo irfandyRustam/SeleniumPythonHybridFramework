773dd14e106cec10c4ae7659b320db1e911488d3
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-07-19 13:42:32 +0800

    generate custom logger

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        print("Launching Chrome Browser ..")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+        print("Launching FireFox Browser ..")
+    else:
+        driver = webdriver.Chrome()
     return driver
+
+def pytest_addoption(parser):   # This will get the value from CLI / hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):   # This will return the Browser value to setup method
+    return request.config.getoption("--browser")
